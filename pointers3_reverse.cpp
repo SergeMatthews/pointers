60808509ae1485968a368967cf1cb2d11d2b8725
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-22 09:51:15 +0600

    Add files via upload

---
+++ pointers3_reverse.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+void swap(int* a, int* b);
+void reverse(int* arr, size_t arr_size);
+void print(int* arr, size_t arr_size);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int arr1[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::cout << "До функции reverse: ";
+    print(arr1, sizeof(arr1)/sizeof(*arr1));
+
+    reverse(arr1, sizeof(arr1) / sizeof(*arr1));
+    std::cout << "После функции reverse: ";
+    print(arr1, sizeof(arr1) / sizeof(*arr1));
+
+    return 0;
+}
+
+void swap(int* a, int* b) {
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+void reverse(int* arr, size_t arr_size) {
+    for (int i = 0, j = arr_size - 1; i < arr_size / 2; ++i, --j)
+    {
+        swap(&arr[i], &arr[j]);
+    }
+}
+void print(int* arr, size_t arr_size) {
+    for (int i = 0; i < arr_size; ++i)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
+}
